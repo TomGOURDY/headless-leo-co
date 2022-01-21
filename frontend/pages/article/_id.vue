52a102a142e9b1443d2cc807584702bd01430a73
TomGOURDY <tom.gourdy@edu.devinci.fr>
2022-01-21 19:56:11 +0100

    updating article-solo page

--- frontend/pages/article/_id.vue
+++ frontend/pages/article/_id.vue
@@ -1,8 +1,30 @@
 <template>
   <div>
-    article-unique
+    <navbar contentId="#content"></navbar>
+    <article id="content">
+      <div class="container">
+          <h1>Ici c'est le titre</h1>
+          <div>Ici c'est le markdown</div>
+          <p>Ici c'est la date de publication</p>
+      </div>
+    </article>
+    <asso-footer></asso-footer>
   </div>
 </template>
+
+<style>
+.container {
+  width: 70%;
+  margin-left: 15%;
+  margin-right: 15%;
+  margin-top: 50px;
+}
+
+p {
+  font-size: 12px;
+  margin-top: 25px;
+}
+</style>
 
 <script>
 export default {
