b7a633d4c44f03f2646a8d893283513e89996797
Arthur TESSE <arthur.tesse@gmail.com>
2022-01-21 19:51:36 +0100

    feat: Add database seeder

---
+++ server/config/functions/seeders.js
@@ -0,0 +1,100 @@
+const articles = [
+  {
+    "title": "Migration des amphibiens - Agissons ensemble !",
+    "content": "En février, nous entrons dans la période de migration des amphibiens qui rejoignent leur site de reproduction : un étang, une mare, un fossé... mais comme chaque année, nombreux sont ceux qui n’arriveront jamais à destination, victimes du trafic routier.\nCrapauds, grenouilles, tritons et salamandres sortent de leur lieu d’hivernage principalement lors des soirées humides et douces des mois de février et mars. Lorsqu’ils sont amenés à traverser une route, celle-ci peut être particulièrement meurtrière en fonction du trafic.\n\nDepuis maintenant près de dix ans, l'Agence régionale pour la biodiversité en Île-de-France procède au recensement des sites d’écrasements grâce à sa plateforme participative de saisie en ligne : [http://amphibiens.arb-idf.fr](link). Plusieurs dizaines de lieux ont ainsi pu être recensés. Cependant, d’autres restent certainement à découvrir, c’est pourquoi nous vous invitons à nous signaler sur la plateforme toute traversée d’amphibiens que vous observez !\n\n![csm_rana_dalmatina_bdbernouille_010315_luciledewulf_2_0_478962abd0.jpg](/uploads/csm_rana_dalmatina_bdbernouille_010315_luciledewulf_2_0_478962abd0_fcd1764eb3.jpg)\n\nPour participer activement à la sauvegarde des amphibiens, cette plateforme recense également les initiatives permettant de faire les faire traverser durant leur migration : dispositifs de traversées temporaires, permanents et fermetures temporaires de route.\n\nChaque année, des bénévoles sont appelés par les différentes structures mettant en place ces dispositifs afin de les installer et de relever les seaux. N’hésitez plus à participer, votre aide est précieuse !\n\nD’années en années, ces dispositifs évoluent, et certains crapaudromes deviennent des ouvrages permanents. En 2020, ce fut le cas du crapaudrome de la Plaine de Sorques installé en prolongement du crapauduc depuis 20 ans ! Le Conseil départemental de Seine-et-Marne a mobilisé 250 bénévoles pour réaliser ce chantier participatif. Un bilan du crapaudrome est disponible ci-contre. Les 900 mètres de bâches, sardines et piquets sont désormais disponibles pour d’autres projets !",
+  },
+  {
+    "title": "Un poisson \"conduit\" son aquarium - Intelligence des poissons rouges",
+    "content": "Un poisson rouge « conduisant » son aquarium – placé sur roulettes – pour se déplacer dans une pièce : cette vidéo illustre la recherche menée par des scientifiques et destinée à évaluer les capacités des poissons dans un milieu différent du leur.\n\nÉvaluer les capacités de navigation des poissons « hors de l’eau » (sans risquer l’asphyxie !) : une étude dont les résultats sont loin d’être anecdotiques. Des chercheurs de l’université Ben Gourion, en Israël, ont conçu une plateforme roulante qu’un poisson peut « conduire » en se déplaçant à l’intérieur son aquarium. Des caméras suivent le mouvement du poisson dans l’eau, et le signal obtenu est envoyé à un ordinateur guidant le véhicule dans la pièce. Ainsi, lorsque le poisson rouge s’approche de l’une des vitres en faisant face à celle-ci, le véhicule avance dans la direction correspondante.\n\n![Fish-on-Wheels (1).png](/uploads/Fish_on_Wheels_1_35cb5f6813.png)\n\n« Cette étude est extrêmement intéressante, même si la capacité de transfert de représentation spatiale chez un individu vivant dans un milieu aquatique vers un environnement terrestre – démontrée ici chez le poisson rouge – n’est pas si surprenante, nuance Violaine Colson, ingénieure de recherche au Laboratoire de Physiologie et de Génomique des Poissons de l’INRAE (Institut national de recherche pour l’agriculture, l’alimentation et l’environnement), contactée par 30millionsdamis.fr. Les poissons d'aquarium voient parfaitement l'environnement terrestre à travers les parois de leur bac et se dirigent vers la paroi du bac où se trouve la personne qui leur distribue de l'aliment. » Un comportement que n’importe quel détenteur de poisson a déjà pu observer ! Toutefois, dans cette étude, les poissons « apprennent que s'ils appuient sur une zone de la paroi en direction d'une cible, ils s'orientent vers la cible pour recevoir une récompense, même si la cible change de place et même si des cibles d’autres couleurs sont présentes, précise la chercheuse. Ils utilisent donc des modalités cognitives spatiales – basées sur des repères visuels présents dans la pièce (environnement terrestre) – pour s'orienter. »\n\n![F4MGBFTSDMI6ZITNDQQ4C2Y4SM.jpgw1440-800x500.jpeg](/uploads/F4_MGBFTSDMI_6_ZITNDQQ_4_C2_Y4_SM_jpgw1440_800x500_e3103fa050.jpeg)\n\nPeut-on, par conséquent, parler d’ « intelligence » chez les poissons ? « Oui, les poissons sont dotés de capacités cognitives complexes », affirme Violaine Colson, énumérant les principales caractéristiques scientifiquement démontrées chez ces animaux : navigation, apprentissage social, mémorisation d'évènements positifs ou aversifs (désagréables, NDLR) « parfois pendant plusieurs mois voire années (pour les évènements aversifs) », mais aussi la reconnaissance individuelle de nombreux congénères, ou encore, la « flexibilité cognitive », c’est-à-dire la capacité de changer de tâche ou de stratégie mentale et à passer d'un comportement à un autre, permettant l'adaptation aux situations nouvelles.\n\n« Pour la flexibilité cognitive, on le voit dans cette étude quand la consigne est inversée (changement de place de la cible) », note la chercheuse, qui a elle-même démontré cette aptitude chez les poissons en réalisant sa propre évaluation. Au laboratoire, des truites reçoivent une récompense lorsqu’elles appuient sur l'une des deux tiges placées devant deux images diffusées sur un écran. Résultat : les truites associent parfaitement l'emplacement de la tige avec l'image récompensée. Plus impressionnant encore, « si on inverse la consigne (l'image non récompensée initialement devient l'image récompensée), elles font l'association inverse en seulement quelques essais », glisse V. Colson. Et si l'on inverse à nouveau la consigne (et ce 4 fois de suite), une seule erreur quand la consigne vient de changer leur permettra ensuite de se corriger ! « Cette flexibilité cognitive (...) montre que les poissons peuvent facilement modifier leur comportement face à tout changement de leur environnement, leur conférant des capacités d'adaptation remarquables », souligne la chercheuse.\n",
+  },
+  {
+    "title": "Retour du lynx en France - Le film \"LYNX\" de Stéphane Geslin",
+    "content": "![Capture d’écran 2022-01-10 à 11.11.18.png](/uploads/Capture_d_ecran_2022_01_10_a_11_11_18_937879c24e.png)\n\nAu cœur du massif jurassien, un appel étrange raisonne à la fin de l'hiver. La superbe silhouette d'un lynx boréal se faufile parmi les hêtres et les sapins. Il appelle sa femelle. En suivant la vie de ce couple et de ses chatons, nous découvrons un univers qui nous est proche et pourtant méconnu... Une histoire authentique dont chamois, aigles, renards et hermines sont les témoins de la vie secrète du plus grand félin d'Europe qui reste menacé... Un film pour découvrir le rôle essentiel que ce discret prédateur occupe dans nos forêts, l'équilibre qu'il a rétabli dans un milieu fragile mais aussi les difficultés qu'il rencontre dans un paysage  largement occupé par les humains.\n\nLa bande annonce : [https://www.youtube.com/watch?v=NCxtKin5k3w](link) !",
+  }
+]
+
+const members = [
+  {
+    "first_name": "Cléa",
+    "last_name": "Coursier",
+    "email": "clea.coursier@edu.devinci.fr",
+    "role": "president",
+  },
+  {
+    "first_name": "Solène",
+    "last_name": "Beauviala",
+    "email": "solene.beauviala@edu.devinci.fr",
+    "role": "vice_president",
+  },
+  {
+    "first_name": "Lucas",
+    "last_name": "Boisseau",
+    "email": "lucas.boisseau@edu.devinci.fr",
+    "role": "responsable_evenements",
+  },
+  {
+    "first_name": "Shahina",
+    "last_name": "Alibay",
+    "email": "shahina.alibay@edu.devinci.fr",
+    "role": "responsable_partenariats",
+  },
+  {
+    "first_name": "Célestine",
+    "last_name": "Moquin",
+    "email": "celestine.moquin@edu.devinci.fr",
+    "role": "secretaire",
+  },
+  {
+    "first_name": "Ariane",
+    "last_name": "Oja",
+    "email": "ariane.oja@edu.devinci.fr",
+    "role": "tresorier",
+  },
+  {
+    "first_name": "Alexandre",
+    "last_name": "James",
+    "email": "alexandre.james@edu.devinci.fr",
+    "role": "responsable_partenariats",
+  },
+  {
+    "first_name": "Thibaud",
+    "last_name": "Mention",
+    "email": "thibaud.mention@edu.devinci.fr",
+    "role": "responsable_communication",
+  },
+  {
+    "first_name": "Léa",
+    "last_name": "Fournier",
+    "email": "lea.fournier@edu.devinci.fr",
+    "role": "responsable_communication",
+  },
+  {
+    "first_name": "Benjamin",
+    "last_name": "Pouts",
+    "email": "benjamin.pouts@edu.devinci.fr",
+    "role": "responsable_evenements",
+  }
+]
+
+module.exports = (async ({services}, isDebug) => {
+  if (!isDebug) return
+
+  await Promise.all(
+    [
+      ...(
+        (await services.member.count()) > 0
+          ? []
+          : members.map((member) =>
+            (() => (services.member.create(member)))()
+          )
+      ),
+      ...(
+        (await services.article.count()) > 0
+          ? []
+          : articles.map((article) =>
+          (() => (services.article.create(article)))()
+        )
+      )
+    ]
+  )
+})
