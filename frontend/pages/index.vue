18e2c4a3f421ac63171da2e886457166e597d620
Antoine75 <antoine.savelli@edu.devinci.fr>
2022-01-21 12:25:42 +0100

    feat: Start index page

--- frontend/pages/index.vue
+++ frontend/pages/index.vue
@@ -1,13 +1,22 @@
 <template>
-  <div>
-    Ici c'est un test pd
-    <UiButton raised>AAA</UiButton>
+    <div>
+      <navbar></navbar>
+
+      <asso-footer></asso-footer>
   </div>
 </template>
 
+<style>
+
+</style>
+
+</template>
+
 <script>
+import Navbar from '../components/Navbar.vue'
+import AssoFooter from '../components/AssoFooter.vue'
 export default {
-  name: 'IndexPage',
+  components: { Navbar , AssoFooter },
   async asyncData (ctx) {
     return {  }
   },
